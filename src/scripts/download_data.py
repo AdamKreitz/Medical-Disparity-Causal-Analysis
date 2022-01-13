d44b40bf3d960ed8666721762975d3d4cf68806a
mglevitt <59936512+mglevitt@users.noreply.github.com>
2022-01-13 10:37:52 -0800

    download data

---
+++ src/scripts/download_data.py
@@ -0,0 +1,12 @@
+import numpy as np
+import requests
+import urllib
+import os
+import logging
+from pathlib import Path
+import pandas as pd
+
+file_name = 'all_cdc_data.csv'
+url = 'https://chronicdata.cdc.gov/resource/g4ie-h725.csv'
+file = requests.get(url)
+open(f'src/data/{file_name}','wb').write(file.content)
